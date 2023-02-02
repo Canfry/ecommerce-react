bef0a0523198b04dafdd940b2a90d0d79efd031c
fry <fry.land@hotmail.fr>
2023-02-02 10:29:09 +0100

    cart page + ProductCartItem

--- src/components/ProductCartItem.jsx
+++ src/components/ProductCartItem.jsx
@@ -14,7 +14,19 @@ export default function ProductCartItem({ product }) {
     //     <div>{product.category}</div>
     //   </div>
     // </div>
-
-    <div>Hello</div>
+    <div className='max-w-[80%] m-auto flex items-center justify-between gap-8'>
+      <div className='w-[200px] h-full'>
+        <img src={product.image} alt='Product image' />
+      </div>
+      <div className='flex flex-col items-start justify-between gap-2 w-[50%]'>
+        <h1 className='text-4xl'>{product.title}</h1>
+        <p className='font-regular text-gray-500'>{product.description}</p>
+      </div>
+      <div>
+        <p>{product.price}</p>
+        <p>{product.rating.rate}</p>
+        <p>{product.category}</p>
+      </div>
+    </div>
   );
 }
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -14,14 +14,21 @@ export default function Cart() {
 
   return (
     <>
-      <div>{count}</div>
+      <div className='max-w-[80%] m-auto flexflex-col items-end justify-center my-8 text-right'>
+        <p className='mb-2'>Total items : {count}</p>
+        <button
+          onClick={onClick}
+          className='rounded-md bg-blue-600 text-white py-1 px-2'
+          disabled={products.length === 0 ? true : false}
+        >
+          Clear Cart
+        </button>
+      </div>
       <div>
         {products.map((product) => (
-          <p key={product.id}>{product.description}</p>
+          <ProductCartItem key={product.id} product={product} />
         ))}
       </div>
-
-      <button onClick={onClick}>Clear</button>
     </>
   );
 }
